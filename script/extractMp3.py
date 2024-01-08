006f043b5530d56eb83e41a840f5b5e8c1058f3a
AHMIA Kenza <ahmiakenza@hotmail.com>
2024-01-08 12:30:04 +0100

    projet RNN

--- script/extractMp3.py
+++ script/extractMp3.py
@@ -1,28 +1,27 @@
 import csv
 
 
-def create_textfiles(row):
-
-    fileName = str(row[0].split('.')[0])
-    with open('output/' +fileName +'.txt', 'w') as f:
-        f.write(row[1])
-        f.close()
-
-
-with open('validated.tsv', 'r') as tsvfile:
-    reader = csv.reader(tsvfile, delimiter='\t')
-    data = []
-    for row in reader:
-        newRow = row[1:3]
-        data.append(newRow)
-        if newRow != ['path', 'sentence']:
-            create_textfiles(newRow)
+def create_textfiles(row): # function to create text files
+    fileName = str(row[0].split('.')[0]) 
+    with open('../corpus/ar/' +fileName +'.txt', 'w') as f: # create text file
+        f.write(row[1]) # write sentence to text file
+        f.close() # close file
 
 
-    with open ('data.tsv', 'a') as csvfile:
-        writer =  csv.writer(csvfile, delimiter="\t")
-        for line in data:
-            writer.writerow(line)
+with open('../corpus/ar/validated.tsv', 'r') as tsvfile: # open tsv file
+    reader = csv.reader(tsvfile, delimiter='\t') # read tsv file
+    data = [] # create empty list
+    for row in reader: # loop through each row
+        newRow = row[1:3] # create new row with only sentence and path
+        data.append(newRow) # append new row to data list
+        if newRow != ['path', 'sentence']: # if new row is not the header row
+            create_textfiles(newRow) # create text file for each row
+
+
+    with open ('data.tsv', 'a') as csvfile: # create new tsv file
+        writer =  csv.writer(csvfile, delimiter="\t") # write tsv file
+        for line in data: # loop through each row
+            writer.writerow(line) # write row to tsv file
 
 
         
